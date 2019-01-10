c0ed8e7397833150c8dc232c1e35f190542379f5
AntoCr <add8596.@gmail.com>
2019-01-11 00:42:53 +0100

    solving klynt version problem

--- miniPlayerData.js
+++ miniPlayerData.js
@@ -1,1 +1,15 @@
-klynt.miniPlayerData = {"downloadAppWording":"Download App","title":"Visioni Future","thanksForWatchingWording":"Thanks for watching!","fullscreenInfoWording":"This program will launch in fullscreen","resumePlaybackWording":"Resume playback?","redirectToMobileApp":"auto","launchAppWording":"Then Launch Project","yesWording":"Yes","url":"","noWording":"No","thumbnail":"Player/css/editor/img/mini-player.jpg","analyticsKey":"","description":"YouTube"}
+klynt.miniPlayerData = {
+    "thanksForWatchingWording": "Thanks for watching!",
+    "resumePlaybackWording": "Resume playback?",
+    "fullscreenInfoWording": "This program will launch in fullscreen",
+    "yesWording": "Yes",
+    "title": "Visioni Future",
+    "thumbnail": "Player/css/editor/img/mini-player.jpg",
+    "noWording": "No",
+    "analyticsKey": "",
+    "downloadAppWording": "Download App",
+    "redirectToMobileApp": "auto",
+    "launchAppWording": "Then Launch Project",
+    "url": "",
+    "description": "YouTube"
+}
